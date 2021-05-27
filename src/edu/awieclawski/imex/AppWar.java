dcc09031bee9126fde10d0450870c0248dbed823
AWieclawski <a_wieclawski@poczta.pl>
2021-05-27 20:01:02 +0200

    Conv. to maven SpringBoot

---
+++ src/edu/awieclawski/imex/AppWar.java
@@ -0,0 +1,19 @@
+package edu.awieclawski.imex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class AppWar extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AppWar.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWar.class, args);
+	}
+}
